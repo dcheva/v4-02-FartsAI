9e26dd7eff4bef4c72bb4dcd139e873df37490b2
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-22 19:28:55 +0300

    Tilemap BG 1280x768 (alt+enter fullscreen vewport expand)

--- main.gd
+++ main.gd
@@ -3,8 +3,13 @@ extends Node2D
 
 func _ready() -> void:
 	pass
-	
+
 
 func _process(delta: float) -> void:
 	pass
-	
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed("alt-enter"):
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (!((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))  ) else Window.MODE_WINDOWED
