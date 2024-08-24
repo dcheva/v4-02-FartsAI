471327aedc53ffbeb783a1b6586b947da1408559
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-25 02:14:23 +0300

    more juicy

--- ai/tasks/choose_random_pos.gd
+++ ai/tasks/choose_random_pos.gd
@@ -9,17 +9,18 @@ extends BTAction
 func _tick(_delta: float) -> Status:
 	if !agent.is_in_group("NPC"): return SUCCESS
 	
-	var dir = ran_dir()
-	var pos = ran_pos(dir)
+	var dir: Vector2 = ran_dir()
+	var pos: Vector2i = ran_pos(dir)
+	print("Move to: %s" % var_to_str(pos))
 	blackboard.set_var(direction_var, dir)
 	blackboard.set_var(position_var, pos)
 	
 	return SUCCESS
 	
-func ran_pos(dir: Vector2) -> Vector2:
+func ran_pos(dir: Vector2) -> Vector2i:
 	var pos = Vector2(randi_range(range_min_in_dir, range_max_in_dir) * dir)
 	pos = pos + agent.get_viewport_rect().size / 2
-	return pos
+	return Vector2i(pos)
 	
 func ran_dir() -> Vector2:
 	var dir = Vector2(randf()-0.5,randf()-0.5).normalized()
